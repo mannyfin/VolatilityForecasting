4be06abf8e7ceae694674a511a5a14571f57ff8d
Alice <hkust20090116@gmail.com>
2017-02-25 13:25:40 -0500

    This is main printing all MSE with clear descriptions, arch added

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from PastAsPresent import *
 from RetCalculator import *
 from sklearn.metrics import mean_squared_error as mse
 from garch_pq_model import garch_model as gm
+from arch_q_model import arch_model as am
 
 
 
@@ -21,20 +22,32 @@ df, df_single_day, df_single_month = read_in_files(filenames)
 days_weeks_months, num_days_per_year = NumDaysWeeksMonths(df=df)
 daily_vol_result = daily_vol_calc(df, df_single_day, num_days_per_year)
 daily_ret = RetCalculator.daily_ret_df(df, df_single_day, num_days_per_year)
+#TODO: add monthly_vol_result
+#TODO: add weekly_vol_result
+
 
 MSE_PastAsPresent = PastAsPresent.today_tomorrow(daily_vol_result)
-plt.show()
+print("Daily PastAsPresent MSE is " + str(MSE_PastAsPresent))
+
 
 one_lag_results = LinRegression.lin_reg(daily_vol_result, 1)
-plt.show()
+print("Daily 1 Lag's MSE is " + str(one_lag_results[0]))
 
 three_lag_results = LinRegression.lin_reg(daily_vol_result, 3)
-plt.show()
+print("Daily 3 Lag's MSE is " + str(three_lag_results[0]))
 
 five_lag_results = LinRegression.lin_reg(daily_vol_result, 5)
-plt.show()
+print("Daily 5 Lag's MSE is " + str(one_lag_results[0]))
+
+ten_lag_results = LinRegression.lin_reg(daily_vol_result, 10)
+print("Daily 10 Lag's MSE is " + str(ten_lag_results[0]))
+
+daily_arch1_mse = am.arch_q_mse(daily_vol_result,np.array(daily_ret['Return_Daily']),1,1)
+print("Daily ARCH(1,1) MSE is :" + str(daily_arch1_mse))
+
 
 daily_garch11_mse = gm.garch_pq_mse(daily_vol_result,np.array(daily_ret['Return_Daily']),1,1,1)
+print("Daily GARCH(1,1) MSE is :" + str(daily_garch11_mse))
 
 
 print("hi")
