2874517298205d9f532e0b478a54fce869529351
Alice <hkust20090116@gmail.com>
2017-02-25 02:36:56 -0500

    This is garch_pq_model

---
+++ src/garch_pq_model.py
@@ -0,0 +1,47 @@
+class garch_model(object):
+    """
+            garch
+
+    :return: weekly_ret
+    """
+    def __init__(self, df):
+        self.df = df
+
+    def garch_pq(ret, p, q,lags):
+
+        from arch import arch_model
+        # The default set of options produces a model with a constant mean, GARCH(1,1) conditional variance and normal errors.
+        garchpq = arch_model(ret,p=p, q=q,lags=lags)
+        res = garchpq.fit(update_freq=1)
+        forecasts = res.forecast()
+        return forecasts.variance['h.1'][1]
+
+    def garch_pq_mse(data,ret,p,q,lags):
+        from sklearn.metrics import mean_squared_error as mse
+
+        garch_pq_forecasts = []
+        for i in range(len(ret)-2):
+            garch_pq_forecasts.append(garch_model.garch_pq(ret[i:(i+lags+1)], p, q,lags))
+        observed = data['Volatility_Daily'][2:]
+        output = mse(observed, garch_pq_forecasts)
+        return output
+
+
+
+
+        # def garch_pq(ret, p, q,lags):
+    #
+    #     from arch import arch_model
+    #     # The default set of options produces a model with a constant mean, GARCH(1,1) conditional variance and normal errors.
+    #     garchpq = arch_model(ret[1:3],p=p, q=q,lags=lags)
+    #     res = garchpq.fit(update_freq=1)
+    #     forecasts = res.forecast()
+    #     print(forecasts.variance)
+    #     # print(res.summary())
+
+
+    # from arch import arch_model
+    # garch11 = arch_model(r, p=1, q=1)
+    # res = garch11.fit(update_freq=10)
+    # print(res.summary())
+
