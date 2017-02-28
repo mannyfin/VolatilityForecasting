0d6a1ca1606bc991f47bc11024da8b8dc0b261e1
manny <scoullos@princeton.edu>
2017-02-28 00:39:42 -0500

    1. Change Volatility Daily to Volatility Time,  and Return Daily to Return Time

--- src/main.py
+++ src/main.py
@@ -15,40 +15,41 @@ import numpy as np
 
 from function_runs import *
 
-filenames = 'AUDUSD.csv'
-# TODO: scale factor for volatility--PLEASE CHECK IF COMPLETED CORRECTLY
+filenames = ['AUDUSD.csv', 'CADUSD.csv', 'CHFUSD.csv', 'EURUSD.csv', 'GBPUSD.csv', 'JPYUSD.csv', 'NOKUSD.csv', 'NZDUSD.csv', 'SEKUSD.csv']
+# TODO output tables after each for loop, or store them somehow
+for name in filenames:
+    # TODO: scale factor for volatility--PLEASE CHECK IF COMPLETED CORRECTLY
 
-#  reads in the files and puts them into dataframes, returns a dataframe called df
-df, df_single_day, df_single_week, df_single_month = read_in_files(filenames)
-# THE LINE BELOW IS NOT REALLY NEEDED IF SCALING IS CONSTANT
-# days_weeks_months, num_days_per_year, num_weeks_per_year, num_months_per_year = NumDaysWeeksMonths(df=df)
+    #  reads in the files and puts them into dataframes, returns a dataframe called df
+    df, df_single_day, df_single_week, df_single_month = read_in_files(name)
+    # THE LINE BELOW IS NOT REALLY NEEDED IF SCALING IS CONSTANT
+    # days_weeks_months, num_days_per_year, num_weeks_per_year, num_months_per_year = NumDaysWeeksMonths(df=df)
 
-daily_vol_result, daily_ret = time_vol_calc(df_single_day)
-weekly_vol_result, weekly_ret = time_vol_calc(df_single_week)
-monthly_vol_result, monthly_ret = time_vol_calc(df_single_month)
+    daily_vol_result, daily_ret = time_vol_calc(df_single_day)
+    weekly_vol_result, weekly_ret = time_vol_calc(df_single_week)
+    monthly_vol_result, monthly_ret = time_vol_calc(df_single_month)
 
-plt.figure(1000)
-plt.plot(daily_vol_result.Date, np.log(daily_vol_result.Volatility_Time))
-# plt.show()
-initial = 0.5
+    plt.figure(1000)
+    plt.plot(daily_vol_result.Date, np.log(daily_vol_result.Volatility_Time))
+    # plt.show()
+    initial = 0.5
 
-fc = FunctionCalls()
-Daily = fc.function_runs('Daily', daily_vol_result, 1, [1, 3, 5, 10],
-                         [np.array(daily_ret['Return_Time']), 1, 0, int(initial*len(daily_ret))],
-                         [np.array(daily_ret['Return_Time']), 1, 1, 0,int(initial*len(daily_ret))])
+    fc = FunctionCalls()
+    Daily = fc.function_runs('Daily', daily_vol_result, 1, [1, 3, 5, 10],
+                             [np.array(daily_ret['Return_Time']), 1, 0, int(initial*len(daily_ret))],
+                             [np.array(daily_ret['Return_Time']), 1, 1, 0,int(initial*len(daily_ret))])
 
-Weekly = fc.function_runs('Weekly', weekly_vol_result, 1, [1, 3, 5, 10],
-                          [np.array(weekly_ret['Return_Time']), 1, 0, int(initial*len(weekly_ret))],
-                           [np.array(weekly_ret['Return_Time']), 1, 1, 0, int(initial*len(weekly_ret))])
+    Weekly = fc.function_runs('Weekly', weekly_vol_result, 1, [1, 3, 5, 10],
+                              [np.array(weekly_ret['Return_Time']), 1, 0, int(initial*len(weekly_ret))],
+                               [np.array(weekly_ret['Return_Time']), 1, 1, 0, int(initial*len(weekly_ret))])
 
-Monthly = fc.function_runs('Monthly', monthly_vol_result, 1, [1, 3, 5, 10],
-                           [np.array(monthly_ret['Return_Time']), 1, 0, int(initial*len(monthly_ret))],
-                           [np.array(monthly_ret['Return_Time']), 1, 1, 0, int(initial*len(monthly_ret))])
+    Monthly = fc.function_runs('Monthly', monthly_vol_result, 1, [1, 3, 5, 10],
+                               [np.array(monthly_ret['Return_Time']), 1, 0, int(initial*len(monthly_ret))],
+                               [np.array(monthly_ret['Return_Time']), 1, 1, 0, int(initial*len(monthly_ret))])
 
+
+    print("yo")
 plt.show()
-print("yo")
-
-
 
 # TODO: add string to each function, such as "GARCH", or "Daily", or "Weekly" for a more generalized plot
 #
