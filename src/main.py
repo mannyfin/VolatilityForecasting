c2fb35d4525e4e26a34137d56aa7f2b427f31ba6
mannycodes <strikker21@gmail.com>
2017-03-01 17:35:09 -0500

    fixed, but doesnt loop

--- src/main.py
+++ src/main.py
@@ -17,11 +17,9 @@ from function_runs import *
 import matplotlib.backends.backend_pdf
 
 # filenames = ['AUDUSD.csv', 'CADUSD.csv', 'CHFUSD.csv', 'EURUSD.csv', 'GBPUSD.csv', 'JPYUSD.csv', 'NOKUSD.csv', 'NZDUSD.csv', 'SEKUSD.csv']
-filenames = ['EURUSD.csv']
+filenames = ['SEKUSD.csv']
 # TODO output tables after each for loop, or store them somehow
 for count, name in enumerate(filenames):
-    # TODO: scale factor for volatility--PLEASE CHECK IF COMPLETED CORRECTLY
-
     #  reads in the files and puts them into dataframes, returns a dataframe called df
     df, df_single_day, df_single_week, df_single_month = read_in_files(name)
     # THE LINE BELOW IS NOT REALLY NEEDED IF SCALING IS CONSTANT
@@ -38,40 +36,41 @@ for count, name in enumerate(filenames):
     plt.title('Daily Vol Result for ' + str(name))
     plt.ylabel('Ln(Volatility)')
     # plt.show()
+
     warmup_period = 10 # set the first 50% of the input data as in-sample data to fit the model
-    plt.figure(1, figsize=(12, 7))
+    plt.figure(3*count+1, figsize=(12, 7))
     fc = FunctionCalls()
     Daily = fc.function_runs(filename=name, stringinput='Daily', warmup=warmup_period, input_data=daily_vol_result[1:],
                              tnplus1=1, lr=[1, 3, 5, 10], arch=[np.array(daily_ret['Return_Time'][1:]), 1, 0],
                              garchpq=[np.array(daily_ret['Return_Time'][1:]), 1, 1, 0])
 
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True, ncol=3)
-    plt.hold(False)
-    plt.figure(2, figsize=(12, 7))
+
+    plt.figure(3*count+2, figsize=(12, 7))
     Weekly = fc.function_runs(filename=name, stringinput='Weekly', warmup=warmup_period,
                               input_data=weekly_vol_result[1:-2], tnplus1=1, lr=[1, 3, 5, 10],
                               arch=[np.array(weekly_ret['Return_Time'][1:-2]), 1, 0],
                               garchpq=[np.array(weekly_ret['Return_Time'][1:-2]), 1, 1, 0])
 
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True, ncol=3)
-    plt.hold(False)
-    plt.figure(3, figsize=(12, 7))
+
+    plt.figure(3*count+3, figsize=(12, 7))
     Monthly = fc.function_runs(filename=name, stringinput='Monthly', warmup=warmup_period, input_data=monthly_vol_result[1:],
                                tnplus1=1, lr=[1, 3, 5, 10], arch=[np.array(monthly_ret['Return_Time'][1:]), 1, 0],
                                garchpq=[np.array(monthly_ret['Return_Time'][1:]), 1, 1, 0])
 
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True, ncol=3)
-    plt.hold(False)
+    # plt.hold(False)
     print("yo")
-plt.show()
-print("yo")
+# plt.show()
+    print("yo")
 """Output multiple plots into a pdf file"""
-pdf = matplotlib.backends.backend_pdf.PdfPages("output.pdf")
+pdf = matplotlib.backends.backend_pdf.PdfPages(name+".pdf")
 # for fig in range(1, len(filenames)*21+1+count): ## will open an empty extra figure :(
 for fig in range(1, 4): ## will open an empty extra figure :(
     pdf.savefig( fig, dpi=1200 )
 pdf.close()
 
-plt.show()
+# plt.show()
 
 print("yo")
