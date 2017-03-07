6ca1e7745747c4772e3a93134c66da56dfb60d19
mannycodes <strikker21@gmail.com>
2017-03-07 17:01:56 -0500

    add temp KNN file

--- src/KNN.py
+++ src/KNN.py
@@ -1,3 +1,14 @@
+# Sebastian Raschka, 2015 (http://sebastianraschka.com)
+# Python Machine Learning - Code Examples
+#
+# Chapter 3 - A Tour of Machine Learning Classifiers Using Scikit-Learn
+#
+# S. Raschka. Python Machine Learning. Packt Publishing Ltd., 2015.
+# GitHub Repo: https://github.com/rasbt/python-machine-learning-book
+#
+# License: MIT
+# https://github.com/rasbt/python-machine-learning-book/blob/master/LICENSE.txt
+
 
 import numpy as np
 from sklearn import datasets
@@ -12,7 +23,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import export_graphviz
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+
+# for sklearn 0.18's alternative syntax
+from distutils.version import LooseVersion as Version
+from sklearn import __version__ as sklearn_version
+if Version(sklearn_version) < '0.18':
+    from sklearn.grid_search import train_test_split
+else:
+    from sklearn.model_selection import train_test_split
+
+#############################################################################
 print(50 * '=')
 print('Section: First steps with scikit-learn')
 print(50 * '-')
@@ -30,22 +50,22 @@ sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
 
+#############################################################################
 print(50 * '=')
-print('Section: K-nearest neighbors - a lazy learning algorithm')
+print('Section: Training a perceptron via scikit-learn')
 print(50 * '-')
 
-knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
-knn.fit(X_train_std, y_train)
+ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+ppn.fit(X_train_std, y_train)
+print('Y array shape', y_test.shape)
 
-plot_decision_regions(X_combined_std, y_combined,
-                      classifier=knn, test_idx=range(105, 150))
+y_pred = ppn.predict(X_test_std)
+print('Misclassified samples: %d' % (y_test != y_pred).sum())
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
 
-plt.xlabel('petal length [standardized]')
-plt.ylabel('petal width [standardized]')
-plt.legend(loc='upper left')
-# plt.tight_layout()
-# plt.savefig('./figures/k_nearest_neighbors.png', dpi=300)
-plt.show()
+
+def versiontuple(v):
+    return tuple(map(int, (v.split("."))))
 
 
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
@@ -99,4 +119,308 @@ plt.legend(loc='upper left')
 
 # plt.tight_layout()
 # plt.savefig('./figures/iris_perceptron_scikit.png', dpi=300)
+plt.show()
+
+#############################################################################
+print(50 * '=')
+print('Section: Logistic regression intuition and conditional probabilities')
+print(50 * '-')
+
+
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+z = np.arange(-7, 7, 0.1)
+phi_z = sigmoid(z)
+
+plt.plot(z, phi_z)
+plt.axvline(0.0, color='k')
+plt.ylim(-0.1, 1.1)
+plt.xlabel('z')
+plt.ylabel('$\phi (z)$')
+
+# y axis ticks and gridline
+plt.yticks([0.0, 0.5, 1.0])
+ax = plt.gca()
+ax.yaxis.grid(True)
+
+# plt.tight_layout()
+# plt.savefig('./figures/sigmoid.png', dpi=300)
+plt.show()
+
+#############################################################################
+print(50 * '=')
+print('Section: Learning the weights of the logistic cost function')
+print(50 * '-')
+
+
+def cost_1(z):
+    return - np.log(sigmoid(z))
+
+
+def cost_0(z):
+    return - np.log(1 - sigmoid(z))
+
+z = np.arange(-10, 10, 0.1)
+phi_z = sigmoid(z)
+
+c1 = [cost_1(x) for x in z]
+plt.plot(phi_z, c1, label='J(w) if y=1')
+
+c0 = [cost_0(x) for x in z]
+plt.plot(phi_z, c0, linestyle='--', label='J(w) if y=0')
+
+plt.ylim(0.0, 5.1)
+plt.xlim([0, 1])
+plt.xlabel('$\phi$(z)')
+plt.ylabel('J(w)')
+plt.legend(loc='best')
+# plt.tight_layout()
+# plt.savefig('./figures/log_cost.png', dpi=300)
+plt.show()
+
+
+#############################################################################
+print(50 * '=')
+print('Section: Training a logistic regression model with scikit-learn')
+print(50 * '-')
+
+
+lr = LogisticRegression(C=1000.0, random_state=0)
+lr.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=lr, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.savefig('./figures/logistic_regression.png', dpi=300)
+plt.show()
+
+print('Predicted probabilities', lr.predict_proba(X_test_std[0, :]
+                                                  .reshape(1, -1)))
+
+#############################################################################
+print(50 * '=')
+print('Section: Tackling overfitting via regularization')
+print(50 * '-')
+
+weights, params = [], []
+for c in np.arange(-5, 5):
+    lr = LogisticRegression(C=10**c, random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+weights = np.array(weights)
+plt.plot(params, weights[:, 0],
+         label='petal length')
+plt.plot(params, weights[:, 1], linestyle='--',
+         label='petal width')
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.legend(loc='upper left')
+plt.xscale('log')
+# plt.savefig('./figures/regression_path.png', dpi=300)
+plt.show()
+
+#############################################################################
+print(50 * '=')
+print('Section: Dealing with the nonlinearly'
+      'separable case using slack variables')
+print(50 * '-')
+
+svm = SVC(kernel='linear', C=1.0, random_state=0)
+svm.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=svm, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.savefig('./figures/support_vector_machine_linear.png', dpi=300)
+plt.show()
+
+#############################################################################
+print(50 * '=')
+print('Section: Solving non-linear problems using a kernel SVM')
+print(50 * '-')
+
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0,
+                       X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+
+plt.scatter(X_xor[y_xor == 1, 0],
+            X_xor[y_xor == 1, 1],
+            c='b', marker='x',
+            label='1')
+plt.scatter(X_xor[y_xor == -1, 0],
+            X_xor[y_xor == -1, 1],
+            c='r',
+            marker='s',
+            label='-1')
+
+plt.xlim([-3, 3])
+plt.ylim([-3, 3])
+plt.legend(loc='best')
+# plt.tight_layout()
+# plt.savefig('./figures/xor.png', dpi=300)
+plt.show()
+
+#############################################################################
+print(50 * '=')
+print('Section: Using the kernel trick to find separating hyperplanes'
+      'in higher dimensional space')
+print(50 * '-')
+
+svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor,
+                      classifier=svm)
+
+plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.savefig('./figures/support_vector_machine_rbf_xor.png', dpi=300)
+plt.show()
+
+
+svm = SVC(kernel='rbf', random_state=0, gamma=0.2, C=1.0)
+svm.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=svm, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.savefig('./figures/support_vector_machine_rbf_iris_1.png', dpi=300)
+plt.show()
+
+
+svm = SVC(kernel='rbf', random_state=0, gamma=100.0, C=1.0)
+svm.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=svm, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.savefig('./figures/support_vector_machine_rbf_iris_2.png', dpi=300)
+plt.show()
+
+
+#############################################################################
+print(50 * '=')
+print('Section: Decision tree learning')
+print(50 * '-')
+
+
+def gini(p):
+    return p * (1 - p) + (1 - p) * (1 - (1 - p))
+
+
+def entropy(p):
+    return - p * np.log2(p) - (1 - p) * np.log2((1 - p))
+
+
+def error(p):
+    return 1 - np.max([p, 1 - p])
+
+x = np.arange(0.0, 1.0, 0.01)
+
+ent = [entropy(p) if p != 0 else None for p in x]
+sc_ent = [e * 0.5 if e else None for e in ent]
+err = [error(i) for i in x]
+
+fig = plt.figure()
+ax = plt.subplot(111)
+for i, lab, ls, c, in zip([ent, sc_ent, gini(x), err],
+                          ['Entropy', 'Entropy (scaled)',
+                           'Gini Impurity', 'Misclassification Error'],
+                          ['-', '-', '--', '-.'],
+                          ['black', 'lightgray', 'red', 'green', 'cyan']):
+    line = ax.plot(x, i, label=lab, linestyle=ls, lw=2, color=c)
+
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15),
+          ncol=3, fancybox=True, shadow=False)
+
+ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+plt.ylim([0, 1.1])
+plt.xlabel('p(i=1)')
+plt.ylabel('Impurity Index')
+# plt.tight_layout()
+# plt.savefig('./figures/impurity.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+
+#############################################################################
+print(50 * '=')
+print('Section: Building a decision tree')
+print(50 * '-')
+
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+tree.fit(X_train, y_train)
+
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined,
+                      classifier=tree, test_idx=range(105, 150))
+
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.savefig('./figures/decision_tree_decision.png', dpi=300)
+plt.show()
+
+# export_graphviz(tree,
+#                out_file='tree.dot',
+#                feature_names=['petal length', 'petal width'])
+
+
+#############################################################################
+print(50 * '=')
+print('Section: Combining weak to strong learners via random forests')
+print(50 * '-')
+
+
+forest = RandomForestClassifier(criterion='entropy',
+                                n_estimators=10,
+                                random_state=1,
+                                n_jobs=2)
+forest.fit(X_train, y_train)
+
+plot_decision_regions(X_combined, y_combined,
+                      classifier=forest, test_idx=range(105, 150))
+
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.savefig('./figures/random_forest.png', dpi=300)
+plt.show()
+
+
+#############################################################################
+print(50 * '=')
+print('Section: K-nearest neighbors - a lazy learning algorithm')
+print(50 * '-')
+
+knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+knn.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=knn, test_idx=range(105, 150))
+
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.savefig('./figures/k_nearest_neighbors.png', dpi=300)
 plt.show()
