72f6a83695182dc19f63f7420b58ca19f92e9f48
mannycodes <strikker21@gmail.com>
2017-03-15 02:29:48 -0400

    VAR_new is just a copy of VAR that I will try to make faster

--- src/LASSO.py
+++ src/LASSO.py
@@ -1,3 +1,9 @@
-from VAR import * as v
+from VAR import x_mat_t_n_qp as xw
+import numpy as np
 
 def lassofn(y, xw, alpha = 0.1):
+    q = 9
+    p = 3
+    n = p * 100
+    LogRV_df = np.log(daily_vol_combined)
+    xw(LogRV_df,q, p, t, n)
