48bcafe56daec9d91269f1f9d0605efffe807b35
mannycodes <strikker21@gmail.com>
2017-03-14 22:42:57 -0400

    fixed day and week issue

--- src/read_in_files.py
+++ src/read_in_files.py
@@ -12,12 +12,19 @@ def read_in_files(file_names):
     os.chdir(os.path.join(os.getenv('userprofile'), 'Desktop/FIN580/Homework1/VolatilityForecasting/src'))
 
     file1 = pd.read_csv(file_names)
+    # file1 = pd.read_csv(file_names, parse_dates=[['Date', 'Time']])
 
     df = pd.DataFrame(file1)
+    # comment out line below
     df.Date = pd.to_datetime(df.Date, format='%m/%d/%Y')
 
+    df['temp'] = df['Date'].astype(str) + ' ' + df['Time']
+    df.temp = pd.to_datetime(df.temp, infer_datetime_format=True)
+    df.temp = df.temp + pd.offsets.Hour(8)
 
-    # this is a timestamp obj
+
+    # sunday 4pm to Friday 4pm
+    # # this is a timestamp obj
     df['year'], df['month'] = df['Date'].dt.year, df['Date'].dt.month
     df['date'] = df['Date'].dt.day
     df['week'] = df['Date'].dt.week
@@ -25,13 +32,28 @@ def read_in_files(file_names):
     """
     these three lines below chunk the data by dates
     """
-    df_single_day = []
-    for group in df.groupby(df.Date, sort=False):
-        df_single_day.append(group[1])
 
+    # df_single_day = []
+    # for group in df.groupby(df.Date, sort=False):
+    #     df_single_day.append(group[1])
+
+
+    g = df.groupby(df['temp'].dt.normalize())
+    df_single_day = []
     df_single_week = []
-    for group in df.groupby(['week', 'year'], sort=False):
-        df_single_week.append(group[1])
+    w = 0
+    for group in g:
+        if len(group[1]) > 1:
+            df_single_day.append(group[1])
+            if (len(df_single_day)-3)%5 is 0 and (len(df_single_day)-3) != 0:
+                # 4th day to
+                df_single_week.append(pd.concat(df_single_day[3+w*5:]))
+
+                w+=1
+
+    # df_single_week = []
+    # for group in df.groupby(['week', 'year'], sort=False):
+    #     df_single_week.append(group[1])
 
     df_single_month = []
     for group in df.groupby(['month', 'year'], sort=False):
