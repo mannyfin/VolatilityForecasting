d822d199af7313f0e0f0c705bb16bff457640c2f
Alice <hkust20090116@gmail.com>
2017-03-14 22:45:58 -0400

    Fixed LR Issue in Indexing

--- src/VAR.py
+++ src/VAR.py
@@ -40,6 +40,7 @@ def get_y(LogRV_df,q, t,n):
         y.append(y_i)
     return y
 
+
 def x_mat_t_n_qp(LogRV_df,q, p, t,n):
     '''
     :param LogRV_df: LogRV_df = np.log(daily_vol_combined)
@@ -57,6 +58,39 @@ def x_mat_t_n_qp(LogRV_df,q, p, t,n):
                 x_t_vec.append(LogRV_df.iloc[t+m-i][k])
         x = x.append([x_t_vec])
     return x
+
+
+# def get_stacked_y(LogRV_df,q, t,n):
+#     y = get_y(LogRV_df,q, t,n)
+#     y_all_in_one = np.array(y).T
+#     return y_all_in_one
+#
+# def get_stacked_x(LogRV_df,q, t,n):
+#     x = x_mat_t_n_qp(LogRV_df,q, p, t,n)
+#     A = np.vstack([x, np.ones(len(x))]).T
+#     x_all_in_one = np.concatenate([x,x,x,x,x,x,x,x,x], axis=0)
+#     return x_all_in_one
+#
+# def predictlogRV_trainingSample_fast(LogRV_df, q, p,n):
+#     PredictedlogRVforAll = []
+#     obs_yforAll = []
+#     y = get_y(LogRV_df, q, t, n)
+#     y = np.array(y).T
+#     x = x_mat_t_n_qp(LogRV_df, q, p, t, n)
+#     x = np.array(x)
+#     A = np.vstack([x, np.ones(len(x))]).T
+#     for t in range(p,int(2/3*len(LogRV_df))-n):
+#         PredictedlogRV = []
+#         obs_y = []
+#         for i in range(q):
+#             params =  np.linalg.lstsq(x, y[i])[0]
+#             x_used_in_pred = x_mat_t_n_qp(LogRV_df, q, p, t + 1, n)
+#             PredictedlogRV.append(np.dot(x_used_in_pred.tail(1), params))
+#             obs_y.append(get_y(LogRV_df, q, t + 1, n)[i][-1])
+#         PredictedlogRVforAll.append(PredictedlogRV)
+#         obs_yforAll.append(obs_y)
+#         print("train_p_" + str(p) + "_t_" + str(t))
+#     return PredictedlogRVforAll, obs_yforAll, b, c
 
 
 '''
@@ -79,19 +113,19 @@ def predictlogRV_trainingSample(LogRV_df, q, p,n):
     for t in range(p,int(2/3*len(LogRV_df))-n):
         x = x_mat_t_n_qp(LogRV_df,q, p, t,n)
         y = get_y(LogRV_df,q, t,n)
+        PredictedlogRV = []
+        obs_y = []
         for i in range(q):
             A = lr()
             A.fit( x, y[i] )
             b = A.coef_
             c = A.intercept_
-            PredictedlogRV = []
-            obs_y = []
             x_used_in_pred = x_mat_t_n_qp(LogRV_df,q, p, t+1,n)
             PredictedlogRV.append( A.predict( x_used_in_pred.tail(1).values.reshape(1, -1) )[0] )
-            obs_y.append(get_y(LogRV_df, q, t, n))
-            obs_y.append(get_y(LogRV_df, q, t + 1, n)[-1])
+            obs_y.append(get_y(LogRV_df, q, t + 1, n)[i][-1])
         PredictedlogRVforAll.append(PredictedlogRV)
         obs_yforAll.append(obs_y)
+        print("train_p_" + str(p) + "_t_" + str(t))
     return PredictedlogRVforAll, obs_yforAll, b, c
 
 
@@ -107,21 +141,22 @@ def predictlogRV_testSample(LogRV_df, q, p,n):
     '''
     PredictedlogRVforAll = []
     obs_yforAll = []
-    for t in range(int(2/3*len(LogRV_df))-n+1,len(LogRV_df)-n):
-        x = x_mat_t_n_qp(LogRV_df,q, p, t,n)
-        y = get_y(LogRV_df,q, t,n)
+    for t in range(int(2/3*len(LogRV_df))-n,len(LogRV_df)-n):
+        x = x_mat_t_n_qp(LogRV_df, q, p, t, n)
+        y = get_y(LogRV_df, q, t, n)
+        PredictedlogRV = []
+        obs_y = []
         for i in range(q):
             A = lr()
-            A.fit( x, y[i] )
+            A.fit(x, y[i])
             b = A.coef_
             c = A.intercept_
-            PredictedlogRV = []
-            obs_y = []
-            x_used_in_pred = x_mat_t_n_qp(LogRV_df,q, p, t+1,n)
-            PredictedlogRV.append( A.predict( x_used_in_pred.tail(1).values.reshape(1, -1) )[0] )
-            obs_y.append(get_y(LogRV_df, q, t + 1, n)[-1])
+            x_used_in_pred = x_mat_t_n_qp(LogRV_df, q, p, t + 1, n)
+            PredictedlogRV.append(A.predict(x_used_in_pred.tail(1).values.reshape(1, -1))[0])
+            obs_y.append(get_y(LogRV_df, q, t + 1, n)[i][-1])
         PredictedlogRVforAll.append(PredictedlogRV)
         obs_yforAll.append(obs_y)
+        print("test_" + str(p) +"_t_" + str(t))
     return PredictedlogRVforAll, obs_yforAll, b, c
 
 '''
@@ -139,13 +174,16 @@ def VAR_MSE_QL(LogRV_df,q,p,n,TrainOrTest):
     :return: MSE, QL and SE plot
     '''
     if TrainOrTest == "Train":
-        PredictedlogRVforAll = predictlogRV_trainingSample(LogRV_df,q, p, n)[0]
+        PredictedlogRVforAll, y = predictlogRV_trainingSample(LogRV_df,q, p, n)[0:2]
         # PredictedlogRVforAll = predictlogRV(LogRV_df,q, p, t,n)[0]
-        y = predictlogRV_trainingSample(LogRV_df, q, p, n)[1]
+        # y = predictlogRV_trainingSample(LogRV_df, q, p, n)[1]
         # y = get_y(LogRV_df,q, p, t,n)
     elif TrainOrTest == "Test":
-        PredictedlogRVforAll = predictlogRV_testSample(LogRV_df, q, p, n)[0]
-        y = predictlogRV_testSample(LogRV_df, q, p, n)[1]
+        PredictedlogRVforAll, y = predictlogRV_testSample(LogRV_df, q, p, n)[0:2]
+        # y = predictlogRV_testSample(LogRV_df, q, p, n)[1]
+
+    y = np.array(y).T
+    PredictedlogRVforAll = np.array(PredictedlogRVforAll).T
 
     Performance_ = PerformanceMeasure()
     MSEforAll = []
@@ -158,7 +196,7 @@ def VAR_MSE_QL(LogRV_df,q,p,n,TrainOrTest):
     mean_MSE = np.mean(MSEforAll)
     mean_QL = np.mean(QLforAll)
 
-    return mean_MSE,mean_QL, MSEforAll, QLforAll
+    return mean_MSE, mean_QL, MSEforAll, QLforAll
 
 '''
     Obtaining optimal p
@@ -206,9 +244,9 @@ def VAR_SE(LogRV_df, q, p_series, data):
     # p = optimal_p(LogRV_df,q,p_series,daily_lookback_series,len_training)
     t= daily_lookback_series[p-1]
     n= p*100
-    PredictedlogRVforAll = predictlogRV(LogRV_df, q, p, n)[0]
+    PredictedlogRVforAll,y = predictlogRV(LogRV_df, q, p, n)[0:2]
     # PredictedlogRVforAll = predictlogRV(LogRV_df, q, p, t, n)[0]
-    y = predictlogRV_trainingSample(LogRV_df, q, p, n)[1]
+    # y = predictlogRV_trainingSample(LogRV_df, q, p, n)[1]
     # y = get_y(LogRV_df, q, t, n)
     # y = get_y(LogRV_df, q, p, t, n)
     label = "VAR"
@@ -217,8 +255,6 @@ def VAR_SE(LogRV_df, q, p_series, data):
         SE(np.sqrt(np.e(y[i])), np.sqrt(np.e(PredictedlogRVforAll[i])), dates, function_method=label)
         # TODO: get dates
         # TODO: add return
-
-
 
 '''
     Using the optimal p on the test sample
@@ -239,128 +275,3 @@ def Test_Sample_MSE_QL(LogRV_df,q,p_series):
     return MSE_QL_optimal_p,MSE_optimal_p_avg,QL_optimal_p_avg,MSE_optimal_p_forAll,QL_optimal_p_forAll
 
 Test_Sample_MSE_QL(LogRV_df = np.log(daily_vol_combined), q=9, p_series=[1,2,3])
-
-#     Construct y as a list of 9 lists.
-#     The k-th list inside y is a series of logRV for the k-th currency pair for k=1,2,...,9
-# '''
-# def get_y(q, p, t,n,indicator):
-#     '''
-#     :param q: q=9 in this project since we have 9 currency pairs
-#     :param p: p is lag
-#     :param t: t = warm-up period
-#     :param n: n = len(LogRV_df)-warmup
-#     :param indicator: indicator is a string input, which can be "Daily", "Weekly" and "Monthly"
-#     :return: y as inputs into LR for all currency pairs
-#     '''
-#     if indicator=="Daily":
-#         indic = 0
-#     elif indicator=="Weekly":
-#         indic = 1
-#     elif indicator=="Monthly":
-#         indic = 2
-#
-#     y = []
-#     for i in range(q):
-#         y_i= []
-#         for k in range(n): # n is the sample size
-#             y_i.append( LogRV_df[indic].iloc[t+k][i] )
-#         y.append(y_i)
-#     return y
-#
-# def x_mat_t_n_qp(q, p, t,n, indicator):
-#     '''
-#     :param q: q=9 in this project since we have 9 currency pairs
-#     :param p: p is lag
-#     :param t: t = warm-up period
-#     :param n: n = len(LogRV_df)-warmup
-#     :param indicator: indicator is a string input, which can be "Daily", "Weekly" and "Monthly"
-#     :return: the x matrix as a input for regression, where the dimension of x is n*(qp)
-#     '''
-#     if indicator=="Daily":
-#         indic = 0
-#     elif indicator=="Weekly":
-#         indic = 1
-#     elif indicator=="Monthly":
-#         indic = 2
-#
-#     x =  pd.DataFrame()
-#     for m in range(n):
-#         x_t_vec = []
-#         for k in range(q):
-#             for i in range(1,p+1):
-#                 x_t_vec.append(LogRV_df[indic].iloc[t+m-i][k])
-#         x = x.append([x_t_vec])
-#     return x
-# '''
-#      Fitting parameters and making prediction based on fitted models
-#      PredictedlogRV collects the predicted logRV for all 9 currency pairs
-#
-# '''
-# def predictlogRV(q,p,t,n,indicator):
-#     '''
-#     :param q: q=9 in this project since we have 9 currency pairs
-#     :param p: p is lag
-#     :param t: t = warm-up period
-#     :param n: n = len(LogRV_df)-warmup
-#     :param indicator: indicator is a string input, which can be "Daily", "Weekly" and "Monthly"
-#     :return: the predicted logRV for all 9 currency pairs
-#     '''
-#     # n = len(LogRV_df)-daily_warmup
-#     x = x_mat_t_n_qp(q=9, p=p, t=t,n=n, indicator=indicator)
-#     PredictedlogRVforAll = []
-#     for i in range(9):
-#         A = lr()
-#         A.fit( x, y[i] )
-#         b = A.coef_
-#         c = A.intercept_
-#         PredictedlogRV = []
-#         for k in range(n):
-#             PredictedlogRV.append( A.predict( x.iloc[k].values.reshape(1, -1) )[0] )
-#         PredictedlogRVforAll.append(PredictedlogRV)
-#     return PredictedlogRVforAll
-#
-#
-# '''
-#     Obtaining MSE and QL
-# '''
-# def VAR_MSE_QL(q,p,t,n,indicator):
-#     '''
-#
-#     :param y: realized logRV for all currency pairs
-#     :param q: q=9 in this project since we have 9 currency pairs
-#     :param p: p is lag
-#     :param t: t = warm-up period
-#     :param n: n = len(LogRV_df)-warmup
-#     :param indicator: indicator is a string input, which can be "Daily", "Weekly" and "Monthly"
-#     :return: MSE, QL and SE plot
-#     '''
-#     # n = len(LogRV_df)-daily_warmup
-#     y = get_y(q, p, t,n, indicator)
-#     PredictedlogRVforAll = predictlogRV(q=q, p=p, t=t,n=n, indicator=indicator)
-#     Performance_ = PerformanceMeasure()
-#     MSEforAll = []
-#     QLforAll = []
-#
-#     for i in range(q):
-#         MSE = Performance_.mean_se(observed=np.sqrt(np.exp(y[i])), prediction=np.sqrt(np.exp(PredictedlogRVforAll[i])))
-#         QL = Performance_.quasi_likelihood(observed=np.sqrt(np.exp(y[i])), prediction=np.sqrt(np.exp(PredictedlogRVforAll[i])))
-#         MSEforAll.append(MSE)
-#         QLforAll.append(QL)
-#     return MSEforAll, QLforAll
-#
-# '''
-#     Obtaining SE plot
-# '''
-#
-# def VAR_SE(q, p, t, n, indicator, data): # can combine with MSE and QL
-#     # n = len(LogRV_df) - daily_warmup
-#     # dates = data['Date'][n:]
-#     PredictedlogRVforAll = predictlogRV(q=q, p=p, t=t,n=n)
-#     label = str(filename) + " " + str(stringinput) + " VAR"
-#     for i in range(q):
-# #         SE(np.sqrt(np.e(y[i])), np.sqrt(np.e(PredictedlogRVforAll[i])), dates, function_method=label)Daily_Vol_df = daily_vol_combined
-#
-#
-# MSE_daily,QL_daliy = VAR_MSE_QL(q=9, p=6, t=daily_warmup, n=len(LogRV_df[0])-daily_warmup,indicator="Daily")
-# MSE_weekly,QL_weekly = VAR_MSE_QL(q=9, p=6, t=weekly_warmup, n=len(LogRV_df[1])-weekly_warmup,indicator="Weekly")
-# MSE_monthly,QL_monthly = VAR_MSE_QL(q=9, p=6, t=monthly_warmup, n=len(LogRV_df[2])-monthly_warmup,indicator="Monthly")
