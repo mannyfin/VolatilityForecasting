0abef81ecf8f7d65af74db1925da4e86c9214c07
Alice <hkust20090116@gmail.com>
2017-03-17 23:20:50 -0400

    Fixed Index Problem. If warmup=400, the first prediction is the one with index 400.

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -23,19 +23,22 @@ class LinRegression:
         :param stringinput is the string that goes on the plot
         """
 
-        data = data.reset_index(range(len(data)))
+        # data = data.reset_index(range(len(data)))
         LogVol = np.log(data['Volatility_Time'])
         PredictedLogVol=[]
         x = [i for i in range(n)]
-        for initial in range(warmup_period, len(LogVol)-1):
+        for initial in range(warmup_period, len(LogVol)):
+        # for initial in range(warmup_period, len(LogVol)-1):
         # for initial in range(warmup_period, len(LogVol)-n):
             for i in range(n):
-                x[i] = LogVol[i:(initial +i-n+1)]
+                x[i] = LogVol[i:(initial +i-n-1)]
+                # x[i] = LogVol[i:(initial +i-n+1)]
                 # x[i] = LogVol[i:(initial +i)]
 
             xstacked = np.column_stack(x)
 
-            y = LogVol[n:initial+1]
+            y = LogVol[n:initial-1]
+            # y = LogVol[n:initial+1]
             # y = LogVol[n:n+initial]
             A = lr()
             A.fit(xstacked, y)
@@ -44,11 +47,13 @@ class LinRegression:
 
             # TODO check that LR is correct
             # # reshape data for prediction
-            PredictedLogVol.append(A.predict(LogVol[initial-n+1 : initial+1].values.reshape(1, -1))[0])
+            PredictedLogVol.append(A.predict(LogVol[initial-n : initial].values.reshape(1, -1))[0])
+            # PredictedLogVol.append(A.predict(LogVol[initial-n+1 : initial+1].values.reshape(1, -1))[0])
             # PredictedLogVol.append(A.predict(LogVol[initial+1:n+initial+1].values.reshape(1, -1))[0])
 
 
-        y = data.Volatility_Time[warmup_period+1:]
+        y = data.Volatility_Time[warmup_period:]
+        # y = data.Volatility_Time[warmup_period+1:]
         # y = data.Volatility_Time[warmup_period+n:]
         PredictedVol = pd.Series(np.exp(PredictedLogVol))
         Performance_ = PerformanceMeasure()
