744d22879e902b8e9ad677f13507e8d0b89b0d35
Alice <hkust20090116@gmail.com>
2017-04-01 17:50:04 -0400

    LR

--- src/LogisticReg_SVM_KernelSVM.py
+++ src/LogisticReg_SVM_KernelSVM.py
@@ -5,6 +5,7 @@ from SEplot import se_plot as SE
 import pandas as pd
 import matplotlib.pyplot as plt
 import forecaster_classifier as fc
+from sklearn.svm import SVC as SVC
 
 # adding labels and obtaining the training and the test samples for a given Delta value
 def Obtain_Traing_Test(df, Delta):
@@ -13,15 +14,15 @@ def Obtain_Traing_Test(df, Delta):
     :param Delta: Delta value which is a candidate of the optimized Delta
     :return: the training and test sample
     """
-    """df['label'] = 0
+    df['label'] = 0
 
     # labeling
     values1 = abs(df.vol_now - df.vol_past * (1 + Delta))
     values2 = abs(df.vol_now - df.vol_past * (1 - Delta))
     condition = values1 < values2
     df.loc[condition, 'label'] = 1
-    df.loc[~condition, 'label'] = -1"""
-    df = fc.forecaster_classifier(df ,{'fxn':fc.volonly,'params':{'delta':Delta}})
+    df.loc[~condition, 'label'] = -1
+    # df = fc.forecaster_classifier(df ,{'fxn':fc.volonly,'params':{'delta':Delta}})
 
     # seperate data into training and test samples
     condition2 = df.V == 1
@@ -32,12 +33,14 @@ def Obtain_Traing_Test(df, Delta):
     return df_training, df_test
 
 # volatility prediction for training/test sample
-def PredictVol(preprocess, Delta, warmup, train_or_test):
+def PredictVol(preprocess, Delta, warmup, train_or_test, model, deg=None):
     """
     :param preprocess: the data frame created in main.py by returnvoldf.py
     :param Delta: Delta value which is a candidate of the optimized Delta
     :param warmup: the number of observations as a warm-up period for the model, which is 400 in our case
     :param train_or_test: a string of "train" or "test"
+    :param model: model can take inputs "LogisticRegression", "SVM", "KernelSVM_poly" ,"KernelSVM_rbf" or "KernelSVM_sigmoid"
+    :param deg: degree of the Kernel SVM when kernel="poly"
     :return: all predicted volatilities
     """
     if train_or_test == "train":
@@ -45,11 +48,23 @@ def PredictVol(preprocess, Delta, warmup, train_or_test):
     elif train_or_test == "test":
         df_whole = Obtain_Traing_Test(preprocess, Delta)[1]
 
+    # specify the type of the model
+    if model =="LogisticRegression":
+        Model = Logit()
+    elif model == "SVM":
+        Model = SVC(kernel='linear')
+    elif model == "KernelSVM_poly":
+        Model = SVC(kernel='poly', degree=deg)
+    elif model == "KernelSVM_rbf":
+        Model = SVC(kernel='rbf')
+    elif model == "KernelSVM_sigmoid":
+        Model = SVC(kernel='sigmoid')
+
+
     PredictedVols = []
     for i in range(np.shape(df_whole)[0]-warmup+2):
         # model fitting and making predictions
         df = df_whole[:warmup-2+i]
-        Model = Logit()
         Model.fit(np.array(df.vol_now).reshape(len(df.vol_now),1), np.array(df.label))
         predicted_y_t = Model.predict(df.vol_future.iloc[-1])
         vol_future_pred = df.vol_future.iloc[-1] *(1+predicted_y_t*Delta)
@@ -59,7 +74,7 @@ def PredictVol(preprocess, Delta, warmup, train_or_test):
     return pd.Series(PredictedVols), pd.Series(vol_future_observed)
 
 # Calculating MSE and QL for training/test sample
-def Logit_MSE_QL(preprocess, Delta,warmup, train_or_test):
+def MSE_QL(preprocess, Delta,warmup, train_or_test, model, deg=None):
     """
     :param preprocess: the data frame created in main.py by returnvoldf.py
     :param Delta: Delta value which is a candidate of the optimized Delta
@@ -69,7 +84,7 @@ def Logit_MSE_QL(preprocess, Delta,warmup, train_or_test):
     """
     # model fitting and making predictions
     Performance_ = PerformanceMeasure()
-    PredictedVols = PredictVol(preprocess, Delta, warmup, train_or_test)
+    PredictedVols = PredictVol(preprocess, Delta, warmup, train_or_test, model, deg)
     prediction = PredictedVols[0]
     observed  = PredictedVols[1]
 
@@ -78,7 +93,7 @@ def Logit_MSE_QL(preprocess, Delta,warmup, train_or_test):
     return MSE, QL,prediction,observed
 
 # optimize in the training sample
-def Optimize(preprocess, DeltaSeq,warmup, filename):
+def Optimize(preprocess, DeltaSeq,warmup, filename, model, deg=None):
     """
     :param preprocess: the data frame created in main.py by returnvoldf.py
     :param DeltaSeq: a sequence of Delta values
@@ -86,8 +101,9 @@ def Optimize(preprocess, DeltaSeq,warmup, filename):
     :return: the optimized Delta
     """
     MSEs = []
+    train_or_test = "train"
     for i in range(len(DeltaSeq)):
-        MSEOutput = Logit_MSE_QL(preprocess, DeltaSeq[i],warmup, train_or_test = "train")[0]
+        MSEOutput = MSE_QL(preprocess, DeltaSeq[i], warmup, train_or_test, model, deg)[0]
         MSEs.append(MSEOutput)
 
     minIndex = MSEs.index(min(MSEs))
@@ -96,17 +112,18 @@ def Optimize(preprocess, DeltaSeq,warmup, filename):
     plt.plot(np.log(DeltaSeq),MSEs)
     plt.xlabel('log(Delta)')
     plt.ylabel('MSE')
-    plt.title(str(filename)+' Delta against MSE')
+    plt.title(str(filename)+' MSE against Delta')
     plt.show()
     return OptimalDelta
 
 
 # measure the prediction performance in the test sample
-def MSE_QL_SE_Test(preprocess,warmup, filename):
+def MSE_QL_SE_Test(preprocess,warmup, filename, model, deg=None):
     DeltaSeq = np.exp(np.linspace(-10, -2, num=100))
-    OptimalDelta = Optimize(preprocess, DeltaSeq,warmup, filename)
+    OptimalDelta = Optimize(preprocess, DeltaSeq,warmup, filename, model, deg)
 
-    Output = Logit_MSE_QL(preprocess, OptimalDelta,warmup, train_or_test = "test")
+    train_or_test = "test"
+    Output = MSE_QL(preprocess, OptimalDelta,warmup, train_or_test, model, deg)
     MSE_test = Output[0]
     QL_test = Output[1]
     prediction  = Output[2]
@@ -120,4 +137,9 @@ def MSE_QL_SE_Test(preprocess,warmup, filename):
 
     return MSE_test, QL_test
 
-MSE_QL_SE_Test(preprocess,warmup=100, filename="AUDUSD")
+#model can take inputs "LogisticRegression", "SVM", "KernelSVM_poly" ,"KernelSVM_rbf" or "KernelSVM_sigmoid"
+TestResult_Logit = MSE_QL_SE_Test(preprocess,warmup=100, filename="AUDUSD", model="LogisticRegression")
+TestResult_SVM = MSE_QL_SE_Test(preprocess,warmup=100, filename="AUDUSD", model="SVM")
+TestResult_KernelSVM_poly = MSE_QL_SE_Test(preprocess,warmup=100, filename="AUDUSD", model="KernelSVM_poly", deg=3)
+TestResult_KernelSVM_rbf = MSE_QL_SE_Test(preprocess,warmup=100, filename="AUDUSD", model="KernelSVM_rbf")
+TestResult_KernelSVM_sigmoid = MSE_QL_SE_Test(preprocess,warmup=100, filename="AUDUSD", model="KernelSVM_sigmoid")
