65cab4e3f89d16ebdc583fbe56def5f83f5f7a8e
Alice <hkust20090116@gmail.com>
2017-04-04 09:30:36 -0400

    Merge branch 'HW3' of https://github.com/mannyfin/VolatilityForecasting into HW3
    # Conflicts:
    #	src/LogisticReg_SVM_KernelSVM.py

--- KNN_original/PPT.py
+++ KNN_original/PPT.py
@@ -3,20 +3,21 @@ from pptx.util import Inches
 from os import listdir
 from os.path import isfile, join
 
-filename = "GBPUSD"
-# modify the path by changing the file name
-mypath ='C:\\Users\\YYJ\\Desktop\\FIN580\\Homework1\\VolatilityForecasting\\src\\'+filename+'\\'
+def Output_to_PPT(filename):
+    # filename = "GBPUSD"
+    # modify the path by changing the file name
+    mypath ='C:\\Users\\YYJ\\Desktop\\FIN580\\Homework1\\VolatilityForecasting\\src\\'+filename+'\\'
 
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-prs = Presentation()
-blank_slide_layout = prs.slide_layouts[6]
+    prs = Presentation()
+    blank_slide_layout = prs.slide_layouts[6]
 
-for i in range(len(onlyfiles)):
-    img_path = mypath+onlyfiles[i]
-    slide = prs.slides.add_slide(blank_slide_layout)
-    left = Inches(0.8)
-    height = Inches(5.5)
-    pic = slide.shapes.add_picture(img_path, left, top, height=height)
+    for i in range(len(onlyfiles)):
+        img_path = mypath+onlyfiles[i]
+        slide = prs.slides.add_slide(blank_slide_layout)
+        left = Inches(0.8)
+        height = Inches(5.5)
+        pic = slide.shapes.add_picture(img_path, left, top, height=height)
 
-prs.save(filename+'.pptx')
+    prs.save(filename+'.pptx')
