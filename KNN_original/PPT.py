383adc9457032076c16d87704878d41f0477d3f8
Alice <hkust20090116@gmail.com>
2017-04-04 09:05:16 -0400

    slides to PPT

---
+++ KNN_original/PPT.py
@@ -0,0 +1,17 @@
+from pptx import Presentation
+from pptx.util import Inches
+
+img_path = 'monty-truth.png'
+
+prs = Presentation()
+blank_slide_layout = prs.slide_layouts[6]
+slide = prs.slides.add_slide(blank_slide_layout)
+
+left = top = Inches(1)
+pic = slide.shapes.add_picture(img_path, left, top)
+
+left = Inches(5)
+height = Inches(5.5)
+pic = slide.shapes.add_picture(img_path, left, top, height=height)
+
+prs.save('test.pptx')
