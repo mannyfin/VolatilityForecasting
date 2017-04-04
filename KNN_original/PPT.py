d4f7ad3cf4cd37b5bff59bbb8c4b1b2560fab436
Alice <hkust20090116@gmail.com>
2017-04-04 09:28:13 -0400

    Merge branch 'HW3' of https://github.com/mannyfin/VolatilityForecasting into HW3
    # Conflicts:
    #	src/LogisticReg_SVM_KernelSVM.py

--- KNN_original/PPT.py
+++ KNN_original/PPT.py
@@ -1,17 +1,22 @@
-from pptx import Presentation
+from pptx import Presentation # pip install python-pptx
 from pptx.util import Inches
+from os import listdir
+from os.path import isfile, join
 
-img_path = 'monty-truth.png'
+filename = "GBPUSD"
+# modify the path by changing the file name
+mypath ='C:\\Users\\YYJ\\Desktop\\FIN580\\Homework1\\VolatilityForecasting\\src\\'+filename+'\\'
+
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 prs = Presentation()
 blank_slide_layout = prs.slide_layouts[6]
-slide = prs.slides.add_slide(blank_slide_layout)
 
-left = top = Inches(1)
-pic = slide.shapes.add_picture(img_path, left, top)
+for i in range(len(onlyfiles)):
+    img_path = mypath+onlyfiles[i]
+    slide = prs.slides.add_slide(blank_slide_layout)
+    left = Inches(0.8)
+    height = Inches(5.5)
+    pic = slide.shapes.add_picture(img_path, left, top, height=height)
 
-left = Inches(5)
-height = Inches(5.5)
-pic = slide.shapes.add_picture(img_path, left, top, height=height)
-
-prs.save('test.pptx')
+prs.save(filename+'.pptx')
