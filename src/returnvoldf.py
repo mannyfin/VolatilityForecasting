8ac9182e4de879fd8925b4149ee81613930acaef
Alice <hkust20090116@gmail.com>
2017-04-06 17:26:19 -0400

    added forecaster 5

--- src/returnvoldf.py
+++ src/returnvoldf.py
@@ -11,10 +11,10 @@ def retvoldf(ret, vol, v):
 
     # this line below creates a DataFrame obj that intersects the dates from vol and v.csv. It then arranges the vols in
     # a time based fashion
-    combined = pd.concat([vol.Date[1:-1], ret.Return_Time[:-2], vol.Volatility_Time[:-2], vol.Volatility_Time[:-1]
+    combined = pd.concat([vol.Date[1:-1], ret.Return_Time[1:-1], vol.Volatility_Time[:-2], vol.Volatility_Time[:-1]
                          .shift(-1), vol.Volatility_Time[:].shift(-2)], axis=1).dropna().set_index('Date')
     preprocess = v.join(combined, how='inner')
-    preprocess.columns = ['V', 'ret_past', 'vol_past', 'vol_now', 'vol_future']
+    preprocess.columns = ['V', 'ret_now', 'vol_past', 'vol_now', 'vol_future']
     # make label column
     preprocess['label'] = 0
 
