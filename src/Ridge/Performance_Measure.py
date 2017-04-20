7696476f163e1eddeae4606eb651e8106a6c6da2
Alice <hkust20090116@gmail.com>
2017-04-20 10:55:31 -0400

    Framework of Project

---
+++ src/Ridge/Performance_Measure.py
@@ -0,0 +1,87 @@
+from sklearn.metrics import mean_squared_error as mse
+import numpy as np
+import pandas as pd
+
+class PerformanceMeasure(object):
+    QL = 0
+    MSE = 0
+
+    def __init__(self, QL = 0, MSE= 0,do_sum=0):
+        self.QL = QL
+        self.MSE = MSE
+        self.do_sum = do_sum
+    def mean_se(self, observed, prediction):
+
+
+
+        if isinstance(observed, pd.core.frame.DataFrame) & isinstance(prediction, pd.core.frame.DataFrame):
+            self.MSE = np.mean(np.square(np.subtract(observed, prediction)))
+            if self.do_sum==1:
+                mse_sum = pd.Series(sum(self.MSE), index=['SumMSE'])
+                self.MSE = self.MSE.append(mse_sum)
+
+            # this line converts to df and transposes from cols to rows
+            self.MSE = pd.DataFrame(self.MSE).T
+            print("MSE is: \n" + str(self.MSE))
+        else:
+            self.MSE = mse(observed, prediction)
+            # self.MSE = self.MSE
+            print("MSE is: " + str(self.MSE))
+
+        return self.MSE
+
+    def quasi_likelihood(self, observed, prediction):
+        """Note: QL DOES NOT WORK IF THERE ARE ZEROES IN DATA SERIES"""
+
+        # if len(prediction.index[prediction<0]) >0:
+        #     observed = observed.drop(observed.first_valid_index() + prediction.index[prediction < 0])
+        #     prediction = prediction[prediction>0]
+
+        if isinstance(observed, pd.core.series.Series) & isinstance(prediction, pd.core.series.Series):
+
+            # the line below will only work if the data is not a dataframe
+            value = observed.ravel() / prediction.ravel()
+            ones = np.ones(len(observed))
+            self.QL = (1 / len(observed)) * (np.sum(value - np.log(value) - ones))
+            print("QL is: " + str(self.QL))
+        elif isinstance(observed,pd.core.frame.DataFrame) & isinstance(prediction,pd.core.frame.DataFrame):
+            value = np.divide(observed,prediction)
+            ones = np.ones(np.shape(observed))
+            # self.QL = (1 / len(observed)) * (np.sum(value - np.log(value) - ones))
+            self.QL = (1 / len(observed)) * (np.sum(value - np.log(value.astype('float64')) - ones))
+            if self.do_sum==1:
+                ql_sum = pd.Series(sum(self.QL), index=['SumQL'])
+                self.QL = self.QL.append(ql_sum)
+
+            # this line converts to df and transposes from cols to rows
+            self.QL = pd.DataFrame(self.QL).T
+
+            print("QL is: \n" + str(self.QL))
+
+        else:
+            # convert prediction to the appropriate data type, either series or dataframe
+            if isinstance(observed,pd.core.frame.DataFrame) is True:
+                prediction = pd.core.frame.DataFrame(prediction)
+
+            #     shouldnt have this issue below, but putting this here just in case
+            elif isinstance(observed, pd.core.series.Series) is True:
+                prediction = pd.core.series.Series(prediction)
+
+            PerformanceMeasure.quasi_likelihood(self, observed=observed, prediction=prediction)
+        # print("QL is: " + str(self.QL))
+        return self.QL
+
+    # def quasi_likelihood(self, observed, prediction):
+    #     """Note: QL DOES NOT WORK IF THERE ARE ZEROES IN DATA SERIES"""
+    #
+    #     if len(prediction.index[prediction<0]) >0:
+    #         observed = observed.drop(observed.first_valid_index() + prediction.index[prediction < 0])
+    #         prediction = prediction[prediction>0]
+    #
+    #     value = prediction.ravel() / observed.ravel()
+    #     ones = np.ones(len(observed))
+    #
+    #
+    #     self.QL = (1 / len(observed)) * (np.sum(value - np.log(value) - ones))
+    #     print("QL is: " + str(self.QL))
+    #     return self.QL
---
+++ src/Ridge/Volatility.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pandas as pd
+
+class Volatility(object):
+    """
+    Creates a Volatility class, which can be used to calculate daily, weekly, or monthly volatility
+    """
+    def __init__(self, df):
+        self.df = df
+
+    def ret(df):
+        """
+                df here contains daily/weekly/monthly 5-minute data
+
+        :return: ret
+        """
+
+        rets = np.log(df['Close'][df.index[-1]]/df['Close'][df.index[0]])
+        # rets = np.sum(np.log(df.Close) - np.log(df.Close.shift(1)))
+        return rets
+
+    def GrowWindowRet(df, initial):
+        """
+                df here contains daily/weekly/monthly 5-minute data
+                initial determines the size of the initial growing window
+
+        :return: GrowWindowRets
+        """
+        GrowWindowRets=[]
+        ret = np.log(df['Close'][df.index[-1]]/df['Close'][df.index[0]])
+
+        return GrowWindowRets
+
+    # @staticmethod
+    def vol_scaling(df):
+        num_days_per_year = 252*288
+        # this was previously daily_vol
+
+        """
+        :pnum_days_per__yearam df: df in the input is a data frame containing data of a pnum_days_per__yearticulnum_days_per__year day
+        :pnum_days_per__yearam n: n is the number of trading days in a pnum_days_per__yearticulnum_days_per__year yenum_days_per__year
+        :return: annualized_vol_scaling
+        """
+
+        vol = np.std(np.log(df.Close) - np.log(df.Close.shift(1)))
+        
+        annualized_vol_scaling = vol * np.sqrt(num_days_per_year)
+        return annualized_vol_scaling
+    
+    
+def time_vol_calc(df_single_time):
+    import pandas as pd
+
+    time_vols = []
+    time_rets = []
+
+    for i in range(len(df_single_time)):
+        time_vols.append(Volatility.vol_scaling(df_single_time[i]))
+        time_rets.append(Volatility.ret(df_single_time[i]))
+
+    # TODO reformat code to include dvol and dret as a single DataFrame
+    # gives an error for df.Date.unique() when using week and month because the num vals are not the same
+    dvol = {'Date': [df_single_time[i]['Date'][df_single_time[i]['Date'].first_valid_index()] for i in range(0, len(df_single_time))], 'Volatility_Time': time_vols}
+    dret = {'Date': [df_single_time[i]['Date'][df_single_time[i]['Date'].first_valid_index()] for i in range(0, len(df_single_time))], 'Return_Time': time_rets}
+
+    # time_vol_result = pd.DataFrame(dvol)
+    time_vol_result = pd.DataFrame([dvol['Date'],dvol['Volatility_Time']]).T
+    time_vol_result.columns = ['Date', 'Volatility_Time']
+
+    # time_ret_result = pd.DataFrame(dret)
+    time_ret_result = pd.DataFrame([dret['Date'],dret['Return_Time']]).T
+    time_ret_result.columns = ['Date', 'Return_Time']
+
+    time_vol_result_zeroes = time_vol_result
+    time_ret_result_zeroes = time_ret_result
+
+    inters_ret = time_ret_result.query('Return_Time == 0').index.values
+    # essentially you will only ever pass through this if statement if there are zero return values
+    if inters_ret.size > 0:
+        inters_vol = time_vol_result.query(('Volatility_Time == 0')).index.values
+        """
+        # this line below removes days where the vols were zero
+        # time_vol_result = time_vol_result.query('Volatility_Time != 0')
+        """
+        # bug point, maybe get an error if c
+        comparison_array = list(set(inters_ret).intersection(inters_vol))
+
+        time_vol_result = time_vol_result.drop(time_vol_result.index[comparison_array])
+        time_ret_result = time_ret_result.drop(time_ret_result.index[comparison_array])
+        time_vol_result.reset_index(drop=True, inplace=True)
+        time_ret_result.reset_index(drop=True, inplace=True)
+    # time_vol_result_zeroes and ret include the zero values...
+    return time_vol_result, time_ret_result, time_vol_result_zeroes, time_ret_result_zeroes
