3b7305e5d2104045ce0777af9752e0620b109544
Alice <hkust20090116@gmail.com>
2017-04-20 15:16:47 -0400

    Return ln(SE) as well

--- src/Ridge/PastAsPresent.py
+++ src/Ridge/PastAsPresent.py
@@ -33,7 +33,7 @@ class PastAsPresent(object):
 
         label = str(filename) + " " + str(stringinput) + " Squared Error PastAsPresent (" + str(1) + ") Volatility"
         """ return a plot of the Squared error"""
-        SE(observed, prediction, dates,function_method=label)
+        # SE(observed, prediction, dates,function_method=label)
         SE = [(observed[i] - prediction[i]) ** 2 for i in range(len(observed))]
         ln_SE = pd.Series(np.log(SE))
 
