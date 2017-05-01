46cf27fc9433576b3854ebe29d34dfd4b8967852
mannycodes <strikker21@gmail.com>
2017-04-30 22:20:22 -0400

    cleanup

--- src/Ridge/main.py
+++ src/Ridge/main.py
@@ -7,7 +7,7 @@ from LRmain import LR
 from RRmain import RR
 from BRRmain import BRR
 from KRRmain import KRR
-from post_processing import KRR_post_process
+from post_processing import KRR_post_process, BRR_post_process
 import os
 
 
@@ -42,19 +42,22 @@ names = []
 for count, name in enumerate(filenames):
     train_set, test_set, name = preprocess(name)
     names.append(name)
-    """
-            PastAsPresent
-    """
-    # pap_mse_list, pap_ql_list = PaP(test_set, name, dictlist)
-    dictlist = PaP(test_set=test_set, train_set=train_set, name=name, count=count, dictlist=dictlist)
 
-    """
-    #         Linear Regression
     # """
+    #         PastAsPresent
+    # """
+    # # pap_mse_list, pap_ql_list = PaP(test_set, name, dictlist)
+    # dictlist = PaP(test_set=test_set, train_set=train_set, name=name, count=count, dictlist=dictlist)
+    #
+
+    # """
+    # #         Linear Regression
+    # # """
     # # lr_mse_list, lr_ql_list, lr_optimal_n_list = LR(train_set, test_set, warmup_period, name,n_seq)
     # dictlist = LR(train_set=train_set, test_set=test_set, warmup_period=warmup_period, name=name, count=count,
     #                n_seq=n_seq, dictlist=dictlist)
     #
+
     # """
     #         Ridge Regression
     # """
@@ -63,6 +66,7 @@ for count, name in enumerate(filenames):
     # dictlist = RR(train_set=train_set, test_set=test_set, warmup_period=warmup_period, name=name, count=count, n_seq=n,
     #               lamda_seq=lamda_seq, lr_optimal_n_list_benchmark=lr_optimal_n_list_benchmark, dictlist=dictlist)
     #
+
     # """
     #         Bayesian Ridge Regression
     # """
@@ -71,6 +75,7 @@ for count, name in enumerate(filenames):
     # dictlist = BRR(train_set, test_set, warmup_period=warmup_period, name=name, count=count, n_seq=n, dictlist=dictlist,
     #                param_range=(param_range1, param_range2, param_range3, param_range4))
 
+
     """
             Kernel Ridge Regression
     """
@@ -78,43 +83,14 @@ for count, name in enumerate(filenames):
     dictlist, kernels = KRR(train_set, test_set, warmup_period=warmup_period, name=name, count=count, n_seq=n, kernels=kernels,
                             dictlist=dictlist, param_range=(krr_alpha, krr_coef0, krr_degree))
 
+# TODO add decorator to functions above to see if they were actually called
+# TODO fix directory issue in post_processing section of code below.
 
 # # Post_processing
 # os.chdir('..')
-# # TODO add decorator to functions above to see if they were actually called
-# """
-#         Bayesian Ridge Regression
-# """
-# brr_test_results_df = pd.DataFrame({"BRR MSE":dictlist['BRR']['brr1_mse_list'],
-#                                      "BRR QL": dictlist['BRR']['brr1_ql_list'],
-#                                    "Optimal alpha1": dictlist['BRR']['brr_optimal_log_alpha1_list'],
-#                                     "Optimal alpha2": dictlist['BRR']['brr_optimal_log_alpha2_list'],
-#                                     "Optimal lambda1": dictlist['BRR']['brr_optimal_log_lambda1_list'],
-#                                     "Optimal lambda2": dictlist['BRR']['brr_optimal_log_lambda2_list']})
-#
-# brr_test_results_df = brr_test_results_df.set_index(np.transpose(names), drop=True)
-# brr_test_results_df.to_csv('BRR_test_MSE_QL.csv')
-#
-# # lnSE to csv
-# brr1_lnSE = pd.DataFrame(dictlist['BRR']['brr1_lnSE_list']).T
-# brr1_lnSE.to_csv('BRR1_lnSE_list.csv')
-#
-# # Predicted vol for each file to csv
-# brr1_PredVol = pd.DataFrame(dictlist['BRR']['brr1_PredVol_list']).T
-# brr1_PredVol.to_csv('BRR1_predictedVol.csv')
-#
+# BRR_post_process(dictlist, names)
 
-"""
-        Kernel Ridge Regression
-"""
-
-# Post_processing
 os.chdir('..')
-# TODO add decorator to functions above to see if they were actually called
-"""
-        Kernel Ridge Regression
-"""
-
-# KRR_post_process(dictlist, names, kernels)
+KRR_post_process(dictlist, names, kernels)
 
 print("Complete")
--- src/Ridge/post_processing.py
+++ src/Ridge/post_processing.py
@@ -46,4 +46,25 @@ def KRR_post_process(dictlist,names,kernels):
         krr1_PredVol = pd.DataFrame(dictlist['KRR'][kernel]['krr1_PredVol_list']).T
         krr1_PredVol.to_csv(str(kernel) + '_KRR1_predictedVol.csv')
 
+def BRR_post_process(dictlist, names):
+    """
+            Bayesian Ridge Regression
+    """
+    brr_test_results_df = pd.DataFrame({"BRR MSE": dictlist['BRR']['brr1_mse_list'],
+                                        "BRR QL": dictlist['BRR']['brr1_ql_list'],
+                                        "Optimal alpha1": dictlist['BRR']['brr_optimal_log_alpha1_list'],
+                                        "Optimal alpha2": dictlist['BRR']['brr_optimal_log_alpha2_list'],
+                                        "Optimal lambda1": dictlist['BRR']['brr_optimal_log_lambda1_list'],
+                                        "Optimal lambda2": dictlist['BRR']['brr_optimal_log_lambda2_list']})
+
+    brr_test_results_df = brr_test_results_df.set_index(np.transpose(names), drop=True)
+    brr_test_results_df.to_csv('BRR_test_MSE_QL.csv')
+
+    # lnSE to csv
+    brr1_lnSE = pd.DataFrame(dictlist['BRR']['brr1_lnSE_list']).T
+    brr1_lnSE.to_csv('BRR1_lnSE_list.csv')
+
+    # Predicted vol for each file to csv
+    brr1_PredVol = pd.DataFrame(dictlist['BRR']['brr1_PredVol_list']).T
+    brr1_PredVol.to_csv('BRR1_predictedVol.csv')
 
